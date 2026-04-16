a0f92c1d53efb8dace57c753c2f7a0a99be71751
juanmurillobol-boop <juan.murillobol@cun.edu.co>
2026-04-16 17:17:44 -0500

    Crer clase persona

--- Index.php
+++ Index.php
@@ -1,3 +1,17 @@
 <?php
+class Persona {
+    public $nombre;
+    public $edad;
 
+    public function __construct($nombre, $edad) {
+        $this->nombre = $nombre;
+        $this->edad = $edad;
+    }
+
+    public function introduce() {
+        return "Hello, my name is " . $this->nombre . " and I am " . $this->edad . " years old.";
+    }
+}
+$persona1 = new Persona("Alice", 30);
+$persona2->mostrar()
 ?>
